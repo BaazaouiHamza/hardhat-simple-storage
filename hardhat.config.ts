46c459d8f62d6df40f661e0ecb8553fed3ff4ac9
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-10-21 14:35:49 +0100

    add verify function to verify the smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config"
+
+const SEPOLIA_RPC_URL = process.env.SEPOLOIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHER_SCAN_API_KEY = process.env.ETHER_SCAN_API_KEY
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    }
+  },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHER_SCAN_API_KEY
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,38 @@
-import { ethers } from "hardhat";
+import { ethers, network, run } from "hardhat"
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.waitForDeployment()
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const simpleStorageAddress = await simpleStorage.getAddress()
+  console.log(`Deployed Contract to :${simpleStorageAddress}`)
+  // what happens when we deploy to our hardhat network (locally) ?
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  if (network.config.chainId === 11155111 && process.env.ETHER_SCAN_API_KEY) {
+    await simpleStorage.deploymentTransaction()?.wait(6)
+    await verify(simpleStorageAddress)
+  }
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress: string) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+    })
+  } catch (e: any) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+main().then(() => process.exit(0)).catch((error) => {
+  console.log(error)
+  process.exit(1)
+})
